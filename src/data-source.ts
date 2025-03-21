e6cd0884f1c630691f85e7589df0621e05c90c97
Angel Maldonado <angel.sosa9110@gmail.com>
2025-03-21 13:04:47 -0300

    fix data souce

--- src/data-source.ts
+++ src/data-source.ts
@@ -9,13 +9,13 @@ const configService = new ConfigService();
 
 export default new DataSource({
   type: 'mysql',
-  host: configService.get('DATABASE_HOST'),
-  port: configService.get<number>('DATABASE_PORT'),
-  username: configService.get('DATABASE_USERNAME'),
-  password: configService.get('DATABASE_PASSWORD'),
-  database: configService.get('DATABASE_DATABASE'),
+  host: configService.get('database.host'),
+  port: configService.get<number>('database.port'),
+  username: configService.get('database.username'),
+  password: configService.get('database.password'),
+  database: configService.get('database.database'),
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
   synchronize: false,
-  logging: configService.get<boolean>('DATABASE_LOGGING'),
+  logging: configService.get<boolean>('database.logging'),
 });
