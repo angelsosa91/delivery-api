a1f385468de95855c52beff413f99867f404231c
Angel Maldonado <angel.sosa9110@gmail.com>
2025-03-21 13:25:54 -0300

    fix ds

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,7 +7,7 @@ config();
 export default new DataSource({
   type: 'mysql', // O el tipo de base de datos que uses
   host: process.env.DB_HOST, // Usar variables de entorno directamente
-  port: parseInt(process.env.DB_PORT!, 10), // Convertir a número
+  port: parseInt(process.env.DB_PORT!, 10), // Convertir a numero
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
@@ -15,5 +15,5 @@ export default new DataSource({
   migrations: [__dirname + '/migrations/*{.ts,.js}'], // Cargar todas las migraciones
   synchronize: process.env.DB_SYNCHRONIZE === 'true', // Convertir a booleano
   logging: process.env.DB_LOGGING === 'true', // Convertir a booleano
-  migrationsRun: process.env.NODE_ENV !== 'development', // Ejecutar migraciones automáticamente en producción
+  migrationsRun: process.env.NODE_ENV !== 'development', // Ejecutar migraciones automaticamente en producción
 });
