1d9288f6768f7d738e785972f418f9af095d5e05
Angel Maldonado <angel.sosa9110@gmail.com>
2025-03-23 22:08:39 -0300

    Update producer service

--- src/qeue/producer/rabbitmq.service.ts
+++ src/qeue/producer/rabbitmq.service.ts
@@ -3,11 +3,12 @@ import * as amqp from 'amqplib';
 
 @Injectable()
 export class RabbitMQService {
+    private readonly mq_host = process.env.RABBIT_MQ_HOST;
     private readonly mq_user = process.env.RABBIT_MQ_USER;
     private readonly mq_pass = process.env.RABBIT_MQ_PASS;
     private readonly mq_port = process.env.RABBIT_MQ_PORT;
 
-    private readonly url = `amqp://${this.mq_user}:${this.mq_pass}@localhost:${this.mq_port}`;
+    private readonly url = `amqp://${this.mq_user}:${this.mq_pass}@${this.mq_host}:${this.mq_port}`;
 
     async sendMessage(queue: string, message: object) {
         const connection = await amqp.connect(this.url);
