9128b62f6961ef218582a4aa5e2a2eaeaf7c12ad
Angel Maldonado <angel.sosa9110@gmail.com>
2025-03-25 17:47:15 -0300

    util module update

--- src/utils/utils.module.ts
+++ src/utils/utils.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GoogleMapsService } from './services/google-maps.service';
 import { ValidationService } from './services/validation.service';
+import { ConfigService } from './services/config.service';
 
 @Module({
-  providers: [GoogleMapsService, ValidationService],
-  exports: [GoogleMapsService, ValidationService],
+  providers: [GoogleMapsService, ValidationService, ConfigService],
+  exports: [GoogleMapsService, ValidationService, ConfigService],
 })
 export class UtilsModule {}
