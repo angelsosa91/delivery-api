16b529b225a530d4d7b96d071be9f6ab1a4dbb3a
Angel Maldonado <angel.sosa9110@gmail.com>
2025-06-11 21:21:20 -0300

    test: update e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,7 +4,7 @@ import * as request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('Application Documentation (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
@@ -16,10 +16,11 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('GET /docs returns 200', () => {
+    return request(app.getHttpServer()).get('/docs').expect(200);
   });
 });
